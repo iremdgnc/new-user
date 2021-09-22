1633a3e9951d923dd05e24986f35e5608726e431
iremdgnc <irem.doganci@hotmail.com>
2021-09-22 09:03:16 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import json
+
+
+def get_user_details():
+    details_list = []
+    count = 0
+    while True:
+        details = input("UserID:"), input("Name:"), input("Age:")
+        details_list.append(details)
+        count += 1
+        if count == 2:
+            return details_list
+
+
+def get_user_details_json_file_write():
+    with open('listfile.txt', 'w') as file:
+        return json.dump(get_user_details(), file)
+
+
+def get_user_details_json_file_read():
+    with open('listfile.txt', 'r') as file:
+        return json.load(file)
+
+
+def my_script():
+    for input in get_user_details_json_file_read():
+        user_id = input[0]
+        user_name = input[1]
+        age = input[2]
+        print (("User ID:" + user_id), ("Name:" + user_name), ("Age:" + age))
+
+
+if __name__ == "__main__":
+    get_user_details_json_file_write()
+    print(get_user_details_json_file_read())
+
